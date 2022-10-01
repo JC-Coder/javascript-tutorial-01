f2b374885657d69be62c59a3f3f79875a89b7dac
JC-Coder <josephchimezie2003@gmail.com>
2022-10-01 11:36:12 +0100

    initial commit

---
+++ class one -- intro to javascript/main.js
@@ -0,0 +1,58 @@
+// Variables 
+
+// variable type , variable name , assignment operator , value to be added 
+
+var name = 'tobi';
+let age = 25;
+const gender = 'male';
+
+// console.log(name, age, gender);
+
+
+// Variable naming - rules 
+/**
+ * 1. variable name must be relevent to the context 
+ * 2. variable name can only start with _, alphabets , $ 
+ * 3. variable name can be plain e.g bookstore, camel case e.g bookStore , snake case e.g book_store
+*/
+
+
+let nameofperson = 'joseph'; // plain naming 
+let nameOfPerson = 'jseoph'; // camel case
+let name_of_person = 'joseph'; // snake case
+
+let $person = 'joseph' 
+let _pseron = 'jospeh'
+
+
+
+
+
+// Concatenation . it is used to join two or more values together. (+)
+
+let summary = 'my name is ' + name + ' i am ' + age + ' years old. I am ' + gender;
+
+// console.log(summary);
+
+
+
+// reassigning values to variable 
+
+name = 'ada';
+// gender = 'female';   // const cannot be modified 
+
+
+// console.log(nameofperson);
+
+
+// Data types 
+
+// string, number (integer) , float , boolean (true, false)
+
+let stringType = 'string';
+let numberType = 25;
+let floatType = 1.2345;
+let booleanType = true;
+
+
+    
