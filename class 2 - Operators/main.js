6b42d632140476d2f4099ade57051db141d9f410
JC-Coder <josephchimezie2003@gmail.com>
2022-10-01 11:39:51 +0100

    added class 2

---
+++ class 2 - Operators/main.js
@@ -0,0 +1,158 @@
+// Operators 
+/**
+ * 1. assignment operators
+ * 2. arithmetic operators
+ * 3. comparision operators
+ * 4. Logical operators
+*/
+
+
+// Assignment operator 
+// equal to (=)
+
+let boy = 'jboy';
+
+
+// Assignment one  => 
+
+/**
+ * 1. story title .
+ * 2. author. 
+ * 3. publication date (year included)
+ * 4. story should be about a person and should have the following variables (name, age, gender)
+ *  */
+
+
+
+// Arithmetic operators
+
+/*
+
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement
+
+*/
+
+let num1 = 10 ;
+let num2 = 5;
+
+
+// addition 
+
+let sumOfNumber = num1 + num2;
+// console.log(sumOfNumber);
+
+
+
+// subtraction 
+
+let subOfNumber = num1 - num2;
+// console.log(subOfNumber);
+
+
+
+// Multiplication 
+
+let mulOfNumber = num1 * num2 ;
+// console.log(mulOfNumber);
+
+
+// Division 
+
+let divOfNumber = num1 / num2;
+// console.log(divOfNumber);
+
+
+
+// Modulus ( use to get the remainder of a number )
+
+let modOfNumber = num1 % num2;
+// console.log(modOfNumber);
+
+
+// Increment 
+
+num1++ ;  // this means increase num1 by one 
+
+num1 += 2; // this means increase num1 by two 
+
+
+// Decreament 
+
+num1--;
+
+num1 -= 3;
+
+// console.log(num1);
+
+
+
+
+// Comparision operators //
+
+
+/*
+
+== equal to 
+=== equal value and equal type 
+!= not equal to 
+!== not equal value and not equal type
+>  greater than 
+<  less than 
+>= greater than or equal to 
+<= less than or equal to 
+? tenary operator 
+ 
+*/
+
+
+let n1 = '30';
+let n2 = 30;
+
+// equal to 
+let result1 = n1 == n2 
+
+let result2 = n1 === n2 // strictly checking to see they are of same value or type 
+
+
+// not equal to 
+let result3 = n1 != n2;
+
+let result4 = n1 !== n2;
+
+// greater than 
+
+let result5 = n1 > n2;
+
+// less than 
+let result6 = n1 < n2;
+
+
+// greater than or equal to 
+let result7 = n1 >= n2 ;
+
+// less than or equal to 
+let result8 = n1 <= n2;
+
+
+// tenary operator 
+let result9 = n1 > n2? 'right' : 'wrong';
+
+console.log(result9);
+
+
+
+
+// Logical Operators //
+
+/*
+&&  and 
+||  or
+!   not
+*/
+
