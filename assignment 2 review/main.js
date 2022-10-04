21762283d3756167dd0ed92659a00e5e06854c5b
JC-Coder <josephchimezie2003@gmail.com>
2022-10-04 14:47:16 +0100

    modified class 5 and 6

---
+++ assignment 2 review/main.js
@@ -0,0 +1,10 @@
+
+let nameOfPerson = prompt('Enter your name');
+let profession = prompt('Enter your profession');
+let gender = prompt('Enter your gender');
+let age = prompt('Enter your age');
+
+
+let summary = 'I know of a good ' +profession+ ' By name ' +nameOfPerson+ ' He is a ' +gender+ ' He is ' +age+ ' Years old.';
+
+console.log(summary);
--- class 5,6 - objects , arrays/main.js
+++ class 5,6 - objects , arrays/main.js
@@ -22,6 +22,7 @@ let car = {
 // }
 
 
+
 // book.summary();
 
 let human = {
